731e89a4275e2516c3e075e9c999cb5c4add0345
Tom <tkremer@stanford.edu>
2016-12-08 20:58:44 -0800

    Wrote FK scores to pickle file

--- Snap_Analytics.py
+++ Snap_Analytics.py
@@ -141,34 +141,60 @@ def measure_comment_lengths():
 	plt.show()
 
 	# return comment_length_count
+def getFKScores():
+
+	# READ FROM PICKLE FILE
+	f = open("FK_reading_ease_scores.pkl", "rb")
+	FK_scores = pickle.load(f)
+	f.close()
+	return FK_scores
+
+def get_FK_histogram():
+	# READ FROM PICKLE FILE
+	f = open("FK_reading_ease_scores.pkl", "rb")
+	FK_scores = pickle.load(f)
+	f.close()
+	return FK_scores
 
 def FK_histogram():
 	from textstat.textstat import textstat
 	print "Measuring Flesch-Kincaid scores..."
 	FK_scores = {}
-	# counter = 0
-	# for comment in Data_Scraper.all_comments:
-	# 	counter += 1
-	# 	if counter %100000 == 0:
-	# 		print str(counter) + " comments done"
-	# 	content = comment.content
-	# 	content = content[3:].replace(" .", ".")
-	# 	if not re.search('[a-zA-Z]', content):
-	# 		continue
+	counter = 0
+	f = open("FK_reading_ease_scores.pkl", "wb")
+	for comment in Data_Scraper.all_comments:
+		counter += 1
+		if counter %100000 == 0:
+			print str(counter) + " comments done"
+		content = comment.content
+		content = content[3:].replace(" .", ".")
+		if not re.search('[a-zA-Z]', content):
+			continue
 
 	# 	""" DO THIS TOO: grade_level = textstat.text_standard(test_data) """
-	# 	fk_score = math.floor(textstat.flesch_reading_ease(content))
+		fk_score = math.floor(textstat.flesch_reading_ease(content))
+		FK_scores[comment.comment_id] = fk_score
+		# f.write(comment.comment_id + "\t" + str(fk_score) + "\n")
+	pickle.dump(FK_scores, f)
+	f.close()
+	print "File written"
+
+	return
+
 	# 	if fk_score in FK_scores:
 	# 		FK_scores[fk_score] += 1
 	# 	else:
 	# 		FK_scores[fk_score] = 1
 
-	# f = open("FK_reading_ease_histogram.txt", "w")
+	# f = open("FK_reading_ease_scores.txt", "w")
 	# for key,value in FK_scores.iteritems():
 	# 	f.write(str(key) + "\t" + str(value) + "\n")
 	# f.close()
 	# print "File written"
 
+
+
+	# READING THE PAGE BACK IN
 	FK_scores = {}
 
 	f = open("FK_reading_ease_histogram.txt", "r")
@@ -240,6 +266,8 @@ def _findDepth(node, g):
 
 # newRoot should be the actual node ID in g.
 # newRoot = mapping.GetKeyId(orig_id)
+
+
 def makeSubGraph(g, newRoot):
 
 	newRoot_NI = g.GetNI(newRoot)
@@ -290,19 +318,22 @@ def main():
 
 	root = getRootNode(mapping, G)
 
+	# FK_histogram()
 
-	""" TRYING TO CREATE A SMALL GRAPH SAMPLE FOR VISUALIZATION """
-	f = open("Mini_graph.csv", "w")
-	thread_id = random.sample(Data_Scraper.thread_ids, 1)
-	newRoot = G.GetNI(mapping.GetKeyId(thread_id)).GetId()
-	# newRoot = mapping.GetKeyId(thread_id)
-	f.write(str(root.GetId()) + "," + str(newRoot) + "\n")
-	sg = makeSubGraph(G, mapping)
-	for edge in sg.Edges():
-		tup = edge.GetId()
-		f.write(str(tup[0]) + "," + str(tup[1]) + "\n")
-	f.close()
-	print "File written"
+	FK_scores = getFKScores()
+
+	# """ TRYING TO CREATE A SMALL GRAPH SAMPLE FOR VISUALIZATION """
+	# f = open("Mini_graph.csv", "w")
+	# thread_id = random.sample(Data_Scraper.thread_ids, 1)
+	# newRoot = G.GetNI(mapping.GetKeyId(thread_id)).GetId()
+	# # newRoot = mapping.GetKeyId(thread_id)
+	# f.write(str(root.GetId()) + "," + str(newRoot) + "\n")
+	# sg = makeSubGraph(G, mapping)
+	# for edge in sg.Edges():
+	# 	tup = edge.GetId()
+	# 	f.write(str(tup[0]) + "," + str(tup[1]) + "\n")
+	# f.close()
+	# print "File written"
 	
 	# for key,value in FK_scores.iteritems():
 	# 	f.write(str(key) + "\t" + str(value) + "\n")
@@ -329,7 +360,8 @@ def main():
 	# print "Nodes: " + str(Data_Scraper.all_comments)
 	# print "Threads: " + str(len(Data_Scraper.thread_ids))
 	# print "Root comments: " + str(len(Data_Scraper.root_comments))
-	
+
+
 
 	print G.GetNodes()
 	print G.GetEdges()
@@ -354,6 +386,11 @@ if __name__ == "__main__":
 
 mapping = snap.TStrIntSH()
 G = snap.LoadEdgeListStr(snap.PNGraph, "politics_edge_list.txt", 0, 1, mapping)
+root = getRootNode(mapping, G)
+thread_sizes = {}
+for thread in root.GetOutEdges():
+		threadsize = _findDepth(g.GetNI(thread), g)
+		thread_sizes[mapping.GetKey(thread)] = threadsize
 
 
 
